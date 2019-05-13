7b46d597034b4e38bbe9a3c6b67ed9f009430fce
Minh-Tam Doan <mtdoaner@gmail.com>
2019-05-13 04:28:10 -0700

    Created login page, allow users to signup and signin

---
+++ Parstagram/ViewControllers/LoginViewController.swift
@@ -0,0 +1,67 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Kay Lab on 5/13/19.
+//  Copyright © 2019 Minh-Tam. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginBoxView: UIView!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginBoxView.layer.cornerRadius = 25
+        loginBoxView.layer.masksToBounds = true
+        signInButton.layer.cornerRadius = 5
+        signInButton.layer.masksToBounds = true
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameTextField.text!
+        let password = passwordTextField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+            
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameTextField.text
+        user.password = passwordTextField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+            
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
